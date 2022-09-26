d483809151ca8177baa57b69bf06661c5ce5673b
Francois Barre <francois.barre@arondor.com>
2022-09-26 23:09:52 +0200

    Add unit test for TraceEventBucket

---
+++ src/test/java/com/arondor/commons/jintruder/TestTraceEventBucket.java
@@ -0,0 +1,30 @@
+package com.arondor.commons.jintruder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestTraceEventBucket
+{
+    @Test
+    public void addEvent()
+    {
+        TraceEventBucket bucket = new TraceEventBucket(23);
+
+        Assert.assertEquals(23, bucket.getThreadId());
+
+        bucket.addEvent(123, 456, true);
+
+        Assert.assertEquals(1, bucket.size());
+        Assert.assertEquals(123, bucket.getMethodId(0));
+        Assert.assertEquals(456, bucket.getTime(0));
+        Assert.assertTrue(bucket.getEnter(0));
+
+        bucket.addEvent(789, 951, false);
+
+        Assert.assertEquals(2, bucket.size());
+        Assert.assertEquals(789, bucket.getMethodId(1));
+        Assert.assertEquals(951, bucket.getTime(1));
+        Assert.assertFalse(bucket.getEnter(1));
+
+    }
+}
